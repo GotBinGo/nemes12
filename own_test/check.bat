da1cffda784f383ce1cab5cdacf20d7f1c2439e1
Tomi <tamas@webtar.hu>
2015-02-15 13:48:48 +0100

    nt2 restores

--- own_test/check.bat
+++ own_test/check.bat
@@ -3,6 +3,7 @@ rmdir \NT2 /s /q
 xcopy .\NT2 \NT2 /e /i /y
 cd bin
 call \NT2\t2.bat
+rmdir \NT2 /s /q
 IF EXIST \NT2_bord xcopy \NT2_bord \NT2 /e /i /y /q
 IF EXIST \NT2_bord rmdir \NT2_bord /s /q
 pause
