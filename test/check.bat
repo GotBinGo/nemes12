f4892aca889598da7b3e3d2a7d20e40f6915ea8c
unknown <bordak.tamas@C115-06.jedlik.helyi>
2015-02-17 12:57:04 +0100

    elore rewrite

--- test/check.bat
+++ test/check.bat
@@ -2,6 +2,7 @@ IF EXIST \NT2 xcopy \NT2 \NT2_bord /e /i /y
 rmdir \NT2 /s /q
 xcopy .\NT2 \NT2 /e /i /y
 cd bin
+timeout 1
 call \NT2\t2.bat
 rmdir \NT2 /s /q
 IF EXIST \NT2_bord xcopy \NT2_bord \NT2 /e /i /y /q
--- work/elore/elore/Program.cs
+++ work/elore/elore/Program.cs
@@ -3,44 +3,39 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-class Program
+namespace elore
 {
-    static void Main(string[] args)
+    class Program
     {
-        var tmp = new List<int>();
-        tmp.Add(0);
-        var a = File.ReadAllLines("elore.be").Select(x => x.Split().Select(y => int.Parse(y)).ToList()).Select((val, i) => new { n = tmp.ToList(), i, val }).ToList();
-        int k = a[0].val[2];
-        a.RemoveAt(0);
-
-        var table = a.Select(x => new { x, f1 = x.val.Max(), f2 = x.val.Average(), f3 = la(x.val, k) });
-        var tn = table.OrderByDescending(x => x.f3).First();
-        for (int i = 0; i < a[0].val.Count; i++)
+        static void Main(string[] args)
         {
-            var col = new List<int>();
-            for (int j = 0; j < a.Count; j++)
-                col.Add(a[j].val[i]);
-
-            var sor = col.Select((val, id) => new { id, val }).OrderByDescending(x => x.val).ToList();
-            var max_or_error = sor[0].val == sor[1].val ? -1 : sor[0].id;
-            if (max_or_error != -1)
-                a[max_or_error].n[0]++;
+            var a = File.ReadAllLines("elore.be").Select(x => x.Split().Select(y => int.Parse(y)).ToList()).ToList();
+            var k = a[0][2];
+            a.RemoveAt(0);
+            using (StreamWriter sw = new StreamWriter("elore.ki"))
+            {
+                var f1 = a.Select((x, i) => new { x = x.Max(), i = i + 1 }).OrderByDescending(x => x.x).First().i;
+                sw.WriteLine(f1);
+                var f2 = a.Select((x, i) => new { x = x.Average(), i = i + 1 }).OrderByDescending(x => x.x).First().i;
+                sw.WriteLine(f2);
+                var f3 = a.Select((x, i) => new { x = la(x, k), i = i + 1 }).OrderByDescending(x => x.x).First();
+                sw.WriteLine(f3.x == -1 ? -1 : f3.i);
+                var cols = a.Select((x, num) => x.Select((y, i) => new { y, i, num })).SelectMany(x => x).GroupBy(x => x.i);
+                var mad = cols.Select(x => new { x.Key, x = x.Select(y => Tuple.Create(y.y, y.num)).ToList(), i = x.Key }).Select(x => new { x.Key, ma = ma(x.x) });
+                var f4 = mad.GroupBy(x => x.ma).OrderByDescending(x => x.Count()).ToList();
+                sw.WriteLine(f4[0].Key == -1 ? -1 : f4[0].Key + 1);
+            }
         }
-        var sor2 = a.Select(x => new { i = x.i, n = x.n[0], val = x.val }).OrderByDescending(x => x.n).ToList();
-        var max_or_error2 = (sor2[0].n == sor2[1].n && 0 == sor2[1].n) ? -1 : sor2[0].i;
-
-        using (StreamWriter sw = new StreamWriter("elore.ki"))
+        static int la(List<int> list, int k)
         {
-            sw.WriteLine(table.OrderByDescending(x => x.f1).First().x.i);
-            sw.WriteLine(table.OrderByDescending(x => x.f2).First().x.i);
-            sw.WriteLine(tn.f3 == -1 ? -1 : tn.x.i);
-            sw.WriteLine(max_or_error2);
+            var p = string.Join("", list.Select(x => x > k ? 1 : 0)).Split('0').Max(x => x.Length);
+            return p == 0 ? -1 : p;
         }
-    }
-    static int la(List<int> list, int k)
-    {
-        var p = string.Join("", list.Select(x => x > k ? 1 : 0)).Split('0').Max(x => x.Length);
-        return p == 0 ? -1 : p;
+        static int ma(List<Tuple<int, int>> list)
+        {
+            var tmp = list.OrderByDescending(x => x.Item1).ToList();
+            var p = tmp[0].Item1 == tmp[1].Item1 ? tmp[0].Item2 : -1;            
+            return p == 0 ? -1 : p;
+        }
     }
 }
-
