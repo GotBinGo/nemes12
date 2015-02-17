2683901e5e86efab9f6e81afd68b8819a44b779b
unknown <bordak.tamas@C102-06.jedlik.helyi>
2015-02-17 09:25:09 +0100

    more

--- work/elore/elore/Program.cs
+++ work/elore/elore/Program.cs
@@ -13,35 +13,34 @@ class Program
         int k = a[0].val[2];
         a.RemoveAt(0);
 
+        var table = a.Select(x => new { x, f1 = x.val.Max(), f2 = x.val.Average(), f3 = la(x.val, k) });
+        var tn = table.OrderByDescending(x => x.f3).First();
+        for (int i = 0; i < a[0].val.Count; i++)
+        {
+            var col = new List<int>();
+            for (int j = 0; j < a.Count; j++)
+                col.Add(a[j].val[i]);
+
+            var sor = col.Select((val, id) => new { id, val }).OrderByDescending(x => x.val).ToList();
+            var max_or_error = sor[0].val == sor[1].val ? -1 : sor[0].id;
+            if (max_or_error != -1)
+                a[max_or_error].n[0]++;
+        }
+        var sor2 = a.Select(x => new { i = x.i, n = x.n[0], val = x.val }).OrderByDescending(x => x.n).ToList();
+        var max_or_error2 = (sor2[0].n == sor2[1].n && 0 == sor2[1].n) ? -1 : sor2[0].i;
+
         using (StreamWriter sw = new StreamWriter("elore.ki"))
         {
-            var table = a.Select(x => new { x, f1 = x.val.Max(), f2 = x.val.Average(), f3 = la(x.val, k) });
             sw.WriteLine(table.OrderByDescending(x => x.f1).First().x.i);
             sw.WriteLine(table.OrderByDescending(x => x.f2).First().x.i);
-            var tn = table.OrderByDescending(x => x.f3).First();
             sw.WriteLine(tn.f3 == -1 ? -1 : tn.x.i);
-
-            for (int i = 0; i < a[0].val.Count; i++)
-            {
-                var col = new List<int>();
-                for (int j = 0; j < a.Count; j++)
-                {
-                    col.Add(a[j].val[i]);
-                }
-                var ap = col.Select((val, id) => new { id, val }).OrderByDescending(x => x.val).ToList();
-                var p = ap[0].val == ap[1].val ? -1 : ap[0].id;
-                if (p != -1)
-                    a[p].n[0]++;
-            }
-            var b = a.Select(x => new { i = x.i, n = x.n[0], val = x.val }).OrderByDescending(x => x.n).ToList();
-            var ki = (b[0].n == b[1].n && 0 == b[1].n) ? -1 : b[0].i;
-            sw.WriteLine(ki);
+            sw.WriteLine(max_or_error2);
         }
     }
     static int la(List<int> list, int k)
     {
         var p = string.Join("", list.Select(x => x > k ? 1 : 0)).Split('0').Max(x => x.Length);
-        return  p == 0 ? -1:  p ;
+        return p == 0 ? -1 : p;
     }
 }
 
